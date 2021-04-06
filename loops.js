bbd4c48cd2292939170f252916a902ec4aa5e03c
Mataxaxa <nikokgr@live.nl>
2021-04-06 19:00:13 +0200

    Opdracht JavaScript Loops Nikolaos Kountouniotis

--- loops.js
+++ loops.js
@@ -6,22 +6,40 @@
 // Hoera!
 // ==========================================
 
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
+
+for (let i = 0; i < 4; i++) {
+    console.log("loop...");
+    if (i === 3) {
+        console.log("klaar!");
+    }
+}
 // Verwachte uitkomsten:
 // loop...
 // loop...
 // loop...
 // loop...
 // klaar!
- // ==========================================
-
+// ==========================================
 
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
+
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(i, "is even");
+    } else if (i % 1 === 0) {
+        console.log(i, "is oneven");
+    }
+}
+
 // Verwachte uitkomsten:
 // 0 is even
 // 1 is oneven
@@ -32,7 +50,6 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
-
 
 
 // ==========================================
@@ -49,4 +66,15 @@
 // >> 7
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++) {
+
+    if (i < 3) {
+        console.log(i);
+    } else if (i < 6) {
+        console.log(">", i);
+    } else if (i > 5) {
+        console.log(">>", i);
+    }
+}
 
